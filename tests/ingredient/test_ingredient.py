d45ae5131304006d558f7aa9a5db266af9432bd5
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-07 08:44:08 -0300

    feat: req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("ovo")
+    ingredient2 = Ingredient("ovo")
+    ingredient3 = Ingredient("salmão")
+
+    assert ingredient.name == "ovo"
+    assert ingredient.restrictions == {Restriction.ANIMAL_DERIVED}
+
+    assert ingredient == ingredient2
+    assert ingredient != ingredient3
+
+    assert hash(ingredient) == hash(ingredient2)
+    assert hash(ingredient) != hash(ingredient3)
+
+    assert repr(ingredient) and repr(ingredient2) == "Ingredient('ovo')"
+    assert repr(ingredient3) == "Ingredient('salmão')"
