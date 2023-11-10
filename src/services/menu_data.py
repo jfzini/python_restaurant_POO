b3fdb4da248298f04cc6c22a616e8c254a9154e7
João Felipe Zini <joaofelipezini@gmail.com>
2023-11-09 22:52:38 -0300

    feat: req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -8,31 +8,24 @@ class MenuData:
 
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
+
         with open(source_path, "r") as sheet:
-            reader = csv.DictReader(sheet)
-            for row in reader:
-                # print(row["recipe_amount"])
+            read_csv = csv.DictReader(sheet)
+
+            for row in read_csv:
                 name = row["dish"]
                 price = float(row["price"])
                 dish = Dish(name, price)
                 ingredient = Ingredient(row["ingredient"])
+
                 if dish not in self.dishes:
                     dish.add_ingredient_dependency(
-                        ingredient, row["recipe_amount"]
+                        ingredient, int(row["recipe_amount"])
                     )
                     self.dishes.add(dish)
                 else:
                     for dish in self.dishes:
                         if dish.name == name:
                             dish.add_ingredient_dependency(
-                                ingredient, row["recipe_amount"]
+                                ingredient, int(row["recipe_amount"])
                             )
-                # dish.add_ingredient_dependency(
-                #     ingredient, row["recipe_amount"]
-                # )
-                # print(dish.get_ingredients())
-                # self.dishes.add(dish)
-
-
-test = MenuData("data/menu_base_data.csv")
-print(test.dishes)
